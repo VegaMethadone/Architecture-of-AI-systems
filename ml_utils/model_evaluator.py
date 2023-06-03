2fee5586929af10d7d735fc2743dde8ef93ca096
radmirkus <radmirka745@gmail.com>
2023-06-03 18:54:30 +0300

    Added new jobs

---
+++ ml_utils/model_evaluator.py
@@ -0,0 +1,9 @@
+from ml_utils.trainer import measure_linear_model, measure_bard_model
+
+if __name__ == '__main__':
+  linear_measured = measure_linear_model()
+  bard_measured = measure_bard_model()
+
+  verdict = 'linear' if linear_measured < bard_measured else 'bard'
+
+  print(verdict)
