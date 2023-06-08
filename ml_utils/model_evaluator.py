7660f7dedd227a0249a6973226cbb28d136f834a
radmirkus <radmirka745@gmail.com>
2023-06-08 13:12:15 +0300

    Added self-hosted runner

--- ml_utils/model_evaluator.py
+++ ml_utils/model_evaluator.py
@@ -1,5 +1,8 @@
 from ml_utils.trainer import measure_linear_model, measure_bard_model
 
+def read():
+  ...
+
 if __name__ == '__main__':
   linear_measured = measure_linear_model()
   bard_measured = measure_bard_model()
