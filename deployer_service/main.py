7c7dcd08a29c0b4796136d6aba2104ab3df1453f
radmirkus <radmirka745@gmail.com>
2023-06-12 10:14:44 +0300

    Fixed docker deploy

--- deployer_service/main.py
+++ deployer_service/main.py
@@ -11,12 +11,23 @@ docker_client = docker.from_env()
 
 @app.post('/deploy_hook')
 def deploy_hook():
-    image_tag = "radmirkus/architecture_ai_rest_service"
+    image_tag = 'radmirkus/architecture_ai_rest_service:latest'
 
+    # Удаление старого контейнера
     containers = docker_client.containers.list(filters={"ancestor": image_tag})
     for container in containers:
-        container.restart()
+        container.stop()
+        container.remove()
+        # container.restart()
         print(f"container {container.id} restarted")
+
+
+    # Загрузка образа
+    image = client.images.pull(image_tag)
+    # Создание контейнера
+    container = client.containers.create(image)
+    # Запуск контейнера
+    container.start()
 
     return { 'status': 'ok' }
 
